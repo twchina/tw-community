4e07f3bad7b9d047cb4721abaf9d8d81624fa249
Bojian Chen <bjchen@thoughtworks.com>
2015-11-03 22:03:37 +0800

    update navibar

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,5 +1,6 @@
 (function (module) {
     module.controller('MainController', ['$scope',function ($scope) {
+        $scope.navList = ['Home', '社区活动', '洞见', '思沃学院', '相关视频', '招聘职位', '其他'];
     }]);
 })(window.module);
 
