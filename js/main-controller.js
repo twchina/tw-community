6be4f33c36fd9be6172f20802297bfdd1a154687
Bojian Chen <bjchen@thoughtworks.com>
2015-11-03 13:24:42 +0800

    make module to be a child of window. Use self-run function. add directive demo

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,3 +1,5 @@
-var module = angular.module('App', []);
-module.controller('MainController', ['$scope',function ($scope) {
-}]);
+(function (module) {
+    module.controller('MainController', ['$scope',function ($scope) {
+    }]);
+})(window.module);
+
---
+++ js/my-directive.js
@@ -0,0 +1,12 @@
+(function (module) {
+    module.directive('myDirective',[function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            template:'<div>hello</div>',
+            link: function (scope, element, attr) {
+                console.log(element);
+            }
+        }
+    }])
+})(window.module);
